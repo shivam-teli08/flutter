74216603d9270afe71cddd1e996846083c4ca48e
shivamteli-08 <shivam.teli.cs@gmail.com>
2025-06-10 17:54:31 +0530

    second day

---
+++ second/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("hi"),
+      ),
+      body: Center(
+        child: Text(
+          'You have pushed the button this many times:',
+          style: TextStyle(
+            fontFamily: 'myfont',
+            fontSize: 24,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
